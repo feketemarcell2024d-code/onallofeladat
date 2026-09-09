8da1607a17d5733bcf6d1984ebd1356ac0ed2ee3
feketemarcell2024d-code <fekete.marcell.2024d@telekimezotur.hu>
2026-09-09 13:20:30 +0200

    adatok

--- onallofeladat/Program.cs
+++ onallofeladat/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Kérem a termék nevét:");
+string termekNev = Console.ReadLine();
+Console.WriteLine("Kérem a termék árát:");
+int EgysegAr = int.Parse(Console.ReadLine());
+Console.WriteLine("Kérem a vásárolt mennyiséget:");
+int vasaroltdb = int.Parse(Console.ReadLine());
